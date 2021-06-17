e964ea3ffa91b69a31939c5e201eb459a10777ef
Evgeniy <evgeniy.raev@zerogravityroom.com>
2021-06-17 22:16:32 +0300

    cleaning up test labels, fixing recource paths to be relative

--- src/wrapper.js
+++ src/wrapper.js
@@ -5,11 +5,10 @@ export default function Wrapper() {
     return (
         <html>
             <head>
-                <script src="/bundle.js" defer ></script>
-                <link rel="stylesheet" href="styles.css"/>
+                <script src="./bundle.js" defer ></script>
+                <link rel="stylesheet" href="./styles.css"/>
             </head>
             <body>
-                it works
                 <CV/>
                 <noscript>
                     You need to enable JavaScript to run this app.
