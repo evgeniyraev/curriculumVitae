1a6f1ad3e90f18dd5ac05accb80a59e82ce5b58f
Evgeniy Raev <evgeniy@raev.dev>
2021-06-18 00:45:47 +0300

    putting some styles

--- src/cv/index.js
+++ src/cv/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './index.scss'; 
 
 export default function CV() {
     return (
         <div
-            className="cv"
+            className="CV"
         >
             Evgeniy Raev
         </div>
