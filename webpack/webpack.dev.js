e433d44f537765fe42a771a76d274ec117253350
Evgeniy <evgeniy.raev@zerogravityroom.com>
2021-06-20 01:25:34 +0300

    cleaning up webpack configs

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -27,42 +27,4 @@ const web = merge(commonConfig, {
     }
 });
 
-const serverConfig = {
-  mode: "development",
-  target: "node",
-    name:"static",
-  node: {
-    __dirname: false,
-  },
-  entry: {
-      "index.js": path.resolve(__dirname, "../src/wrapper.js"),
-  },
-  module: {
-      rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "css-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(j|t)sx?$/,
-                use:[
-                    {
-                        loader: 'babel-loader',
-                    },
-                ]
-            }
-      ],
-  },
-  output: {
-      path: path.resolve(__dirname, "../dist"),
-      filename: "[name]",
-  },
-};
-
-module.exports = [
-    serverConfig,
-    web,
-]
+module.exports = web
