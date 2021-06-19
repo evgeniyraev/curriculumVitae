d68da4d082e4a36e5c987ab2ff6d6401c5638700
Evgeniy <evgeniy.raev@zerogravityroom.com>
2021-06-19 18:51:45 +0300

    moving css esport to webpack

--- src/cv/index.js
+++ src/cv/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import './index.scss'; 
+import './index.scss';
+
+import Projects from './Projects';
 
 export default function CV() {
     return (
@@ -7,6 +9,7 @@ export default function CV() {
             className="CV"
         >
             Evgeniy Raev
+            <Projects/>
         </div>
     )
 }
