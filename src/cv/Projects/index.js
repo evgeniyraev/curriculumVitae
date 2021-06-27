7023d1203fa1ddd309b87e0a0453fa479bd09a5b
Evgeniy <evgeniy.raev@zerogravityroom.com>
2021-06-28 00:42:43 +0300

    adding projects with simple visualization

--- src/cv/Projects/index.js
+++ src/cv/Projects/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Project from './Project';
 import './index.scss';
 
-export default function Projects() {
+export default function Projects({data}) {
     return (
         <div
             className="Projects"
         >
-            List of all projects
+            Projects:
+            { data.map((project, i) => (
+                <Project
+                    key={i}
+                    { ...project }
+                />
+            ))}
         </div>
     )
 }
