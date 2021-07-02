fff254a8362dc5c6b1c52d6884bd2a1d93c2d455
Evgeniy <evgeniy.raev@zerogravityroom.com>
2021-07-02 21:28:14 +0300

    adding font and contact info

--- src/cv/index.js
+++ src/cv/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import './index.scss';
 
 import Projects from './Projects';
+import Contacts from './Contacts';
 import { projects } from './data';
 
 export default function CV() {
@@ -15,8 +16,10 @@ export default function CV() {
                 >
                     <img className="photo"/>
                 </div>
+                <Contacts/>
                 <h1>Evgeniy Raev</h1>
                 <p>I have 11 years of experince in Front-end mobile, desktop and web development, and in the past 4 years my responsobilaties have grown to lead and expand a team of 7 decelopers for creating an enterprise solution. I also have the oprotunaty to conduct technical trainings for juniors and mentor them individually. I am passioned about working on complex and challenging projects seeming impossible or too expensive. I have experice in UX and gathering technical requirements based on client requests or concept designs.</p>
+
             </header>
 
             <Projects
