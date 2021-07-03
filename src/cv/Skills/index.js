fffb906dd18c518b08224f758e50e46f322bfdf9
Evgeniy Raev <evgeniy@raev.dev>
2021-07-03 13:23:10 +0300

    adding edication section, minor skile changes

--- src/cv/Skills/index.js
+++ src/cv/Skills/index.js
@@ -3,104 +3,109 @@ import './index.scss'
 
 export default function Skills() {
     return(
-        <article
-            className="Skills"
+        <div
         >
             <h2>
                 Skills
             </h2>
-            {[
-                {
-                    title:"Languages",
-                    list:[
-                        "javascript",
-                        "swift",
-                        "bash",
-                        "mysql",
-                        "Java (Android)",
-                        "typescript",
-                    ]
-                },
-                {
-                    title:"Frameworks / Enviroments",
-                    list:[
-                        "ReactJs",
-                        "ReactNative",
-                        "NextJs",
-                        "Express",
-                        "PixiJS",
-                        "WebGL",
-                        "websocket",
-                        "TreeJS",
-                        "Electron",
-                        "node",
-                        "UIKit",
-                        "GameKit",
-                        "SpriteKit",
-                        "MetalKit",
-                        "SwiftUI",
-                        "docker",
-                        "PWA",
-                        "jenkins",
-                        "nginx"
-                    ]
-                },
-                {
-                    title:"Software",
-                    list:[
-                        "Photoshop",
-                        "Illustrator",
-                        "XD",
-                        "inDesign",
-                        "AutoCAD",
-                        "figma",
-                        "vim",
-                        "ssh",
-                        "tmux",
-                    ],
-                },
-                {
-                    title:"Services",
-                    list:[
-                        "AWS",
-                        "Azure",
-                        "iTunes Connect",
-                        "Adobe Developer Console",
-                        "Cloud Kit",
-                        "PlayStore Console",
-                        "Google Analytics",
-                    ],
-                },
-                {
-                    title:"Project Managment / Team Lead / SoftSkills",
-                    list:[
-                        "mentoring",
-                        "leading",
-                        "researching",
-                        "design / UX",
-                        "",
+            <article
+                className="Skills"
+            >
+                {[
+                    {
+                        title:"Languages",
+                        list:[
+                            "javascript",
+                            "swift",
+                            "bash",
+                            "mysql",
+                            "Java (Android)",
+                            "typescript",
+                        ]
+                    },
+                    {
+                        title:"Frameworks / Enviroments",
+                        list:[
+                            "ReactJs",
+                            "ReactNative",
+                            "NextJs",
+                            "Express",
+                            "PixiJS",
+                            "WebGL",
+                            "websocket",
+                            "TreeJS",
+                            "Electron",
+                            "node",
+                            "UIKit",
+                            "GameKit",
+                            "SpriteKit",
+                            "MetalKit",
+                            "SwiftUI",
+                            "PWA",
+                        ]
+                    },
+                    {
+                        title:"Design",
+                        list:[
+                            "Photoshop",
+                            "Illustrator",
+                            "XD",
+                            "inDesign",
+                            "AutoCAD",
+                            "figma",
+                        ],
+                    },
+                    {
+                        title:"Tools",
+                        list:[
+                            "vim",
+                            "ssh",
+                            "tmux",
+                            "docker",
+                            "nginx",
+                            "jenkins",
+                        ]
+                    },
+                    {
+                        title:"Services",
+                        list:[
+                            "AWS",
+                            "Azure",
+                            "iTunes Connect",
+                            "Adobe Developer Console",
+                            "Cloud Kit",
+                            "PlayStore Console",
+                            "Google Analytics",
+                        ],
+                    },
+                    {
+                        title:"Soft Skills",
+                        list:[
+                            "project managment",
+                            "team lead",
+                            "mentoring",
+                            "leading",
+                            "researching",
+                            "design / UX",
+                            "architecture",
 
-                    ]
-                },
-                {
-                    title:"Education / Other",
-                    list:[
-                        "Sofia University St. Kliment Ohridski - Computer Science",
-                        "University of Architecture, Civil Engineering  and Geodesy - Construction Engineering",
-                        "Professional Hight school of Civil Engineering and Geodesy - Construction and Architecture",
-                        "drawing",
-                        "english",
-                    ]
-                },
+                        ]
+                    },
+                    {
+                        title:"other",
+                        list:[
+                            "drawing",
+                            "recreational mathematics"
+                        ]
+                    },
 
-            ].map(({title, list}, i) => (
-                <section
-                    key={i}
-                >
-                    <h3>
-                        { title }
-                    </h3>
-                    <div>
+                ].map(({title, list}, i) => (
+                    <section
+                        key={i}
+                    >
+                        <h3>
+                            { title }
+                        </h3>
                         {list.map((el, i) => (
                             <span
                             key={i}
@@ -108,9 +113,9 @@ export default function Skills() {
                                 {el}
                         </span>
                         ))}
-                    </div>
-                </section>
-            ))}
-        </article>
+                    </section>
+                ))}
+            </article>
+        </div>
     )
 }
